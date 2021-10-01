925bea23604e931a7812111d979110f9f8351e49
Emiliano Urbina <emiliano.urbina12171@est.uca.edu.ni>
2021-10-01 07:23:01 -0600

    pathways done y badges gotten (I add an screenshot)

--- Scrollable1/app/src/main/java/ni/edu/uca/scrollable1/MainActivity.kt
+++ Scrollable1/app/src/main/java/ni/edu/uca/scrollable1/MainActivity.kt
@@ -2,7 +2,6 @@ package ni.edu.uca.scrollable1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ni.edu.uca.scrollable1.adapter.ItemAdapter
 import ni.edu.uca.scrollable1.data.Datasource
@@ -12,8 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView: TextView = findViewById(R.id.txtMain)
-        textView.text = Datasource().loadAffirmations().size.toString()
         val myDataSet = Datasource().loadAffirmations()
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter = ItemAdapter(this, myDataSet)
