9c91a2d129b782fdf64c96aa7123c1612ca38880
Emiliano Urbina <emiliano.urbina12171@est.uca.edu.ni>
2021-10-01 06:53:12 -0600

    Almost final version

--- Scrollable1/app/src/main/java/ni/edu/uca/scrollable1/MainActivity.kt
+++ Scrollable1/app/src/main/java/ni/edu/uca/scrollable1/MainActivity.kt
@@ -2,10 +2,21 @@ package ni.edu.uca.scrollable1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import ni.edu.uca.scrollable1.adapter.ItemAdapter
+import ni.edu.uca.scrollable1.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.txtMain)
+        textView.text = Datasource().loadAffirmations().size.toString()
+        val myDataSet = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataSet)
+        recyclerView.setHasFixedSize(true)
     }
 }
