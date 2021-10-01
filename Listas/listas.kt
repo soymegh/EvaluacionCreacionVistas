90ad1b6ec7d0e7659b639db94cccb6ce94ea6f18
Emiliano Urbina <emiliano.urbina12171@est.uca.edu.ni>
2021-10-01 05:30:07 -0600

    Scrollable folder added

--- Listas/listas.kt
+++ Listas/listas.kt
@@ -1,4 +1,5 @@
 fun main(){
-    val numbers: List<Int> = listOf(1, 2, 3, 4, 5, 6)
-    println("List: $numbers")
+    val entrees: MutableList<String> = mutableListOf()
+    entrees.add("noddles")
+    println("${entrees.remove("noddles")}")
 }
